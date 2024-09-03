688d71b62430aa8ea65079cdc87ad32cdfcd9ff8
valerrrich <161199056+valerrrich@users.noreply.github.com>
2024-09-03 07:39:45 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,25 +1,13 @@
-        for (int i = 0; i < 10; i++) {
             data[i] = (int) (Math.random() * 100) + 1;
+
 }
+    public static void main(String[] args) {
+    }
+        for (int i = 0; i < 10; i++) {
+        }
+
         int[] data = new int[10];
         }
         for (int item : data) {
-    public static void main(String[] args) {
-            System.out.println("Random Number: " + item);
-
-    }
 public class RandomDataGenerator {
-
-        }        }
-public class RandomDataGenerator {
-        int[] data = new int[10];
-    }
-    public static void main(String[] args) {
-        }
-        for (int i = 0; i < 10; i++) {
-
-}
-
             System.out.println("Random Number: " + item);
-        for (int item : data) {
-            data[i] = (int) (Math.random() * 100) + 1;
